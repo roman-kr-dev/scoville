b405b16cac638f0469d547eeb1339039a803beaf
Roman Kr <roman7@gmail.com>
2020-12-15 22:56:48 +0200

     first game version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,123 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import classNames from 'classnames';
+import createGradiant from './utils/colors';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { time } from 'console';
+
+enum PepperStates {
+  EMPTY,
+  ACTIVE,
+  SPICY,
+  SWEET
+}
+
+enum KeyboardArrows {
+  LEFT = 'ArrowLeft',
+  RIGHT = 'ArrowRight'
+}
+
+interface UpdatePepperProps {
+  xIndex: number,
+  state: PepperStates
+}
+
+interface PepperProps {
+  state: PepperStates,
+  xIndex: number,
+  onUpdate: ({ xIndex, state }: UpdatePepperProps) => void
+}
+
+interface PlayerProps {
+  color: string
+}
+
+const Pepper = ({ state, xIndex, onUpdate }: PepperProps) => {
+  useEffect(() => {
+    let timeout: number;
+
+    const randomState = () => {
+      timeout = window.setTimeout(() => {
+        const newState = Math.floor(Math.random() * 2) ? PepperStates.SPICY : PepperStates.SWEET;
+
+        if ( newState !== state ) { onUpdate({ xIndex, state: newState }) }
+      }, (Math.random() * 5 * 1000));
+    }
+
+    randomState();
+
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [state, xIndex, onUpdate]);
+
+  return <div className={classNames('cell', {
+    'spicy': state === PepperStates.SPICY,
+    'sweet': state === PepperStates.SWEET
+  })} />
+}
+
+const Player = ({ color }: PlayerProps) => {
+  return <div className="cell player" style={ {backgroundColor: color} } />;
+}
 
 function App() {
+  const [ matrix, setMatrix ] = useState( generateMatrix );
+  const [ colorMatrix ] = useState( generateColorMatrix(matrix.length) );
+  const [ x, setX ] = useState(() => Math.floor(Math.random() * matrix.length));
+  const [ score, setScore ] = useState(0);
+
+  const updatePepperState = useCallback(({ xIndex, state }: UpdatePepperProps) => {
+    setMatrix((currentMatrix) => {
+      const newMatrix = [...currentMatrix];
+      newMatrix[ xIndex ] = {...newMatrix[ xIndex ], state };
+
+      return newMatrix;
+    });
+  }, []);
+
+
+  useEffect(() => {
+    const onKeyDown = (e: KeyboardEvent) => {
+      switch ( e.code ) {
+        case KeyboardArrows.LEFT:
+          setX(currentX => Math.max(0, currentX - 1));
+
+          setScore( score => score + 1 );
+          break;
+
+        case KeyboardArrows.RIGHT:
+          setX(currentX => Math.min(matrix.length - 1, currentX + 1));
+
+          setScore( score => score + 1 );
+          break;
+      }
+    }
+
+    window.addEventListener('keydown', onKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', onKeyDown);
+    }
+  }, [matrix.length]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="scoville-race">
+       {matrix.map(({ state }, i) =>
+          x === i ? <Player key={i} color={ colorMatrix[ score ] } /> : <Pepper key={i} xIndex={i} state={ state } onUpdate={ updatePepperState } />
+       )}
     </div>
   );
+}
+
+function generateMatrix() {
+  return Array(10).fill({state: PepperStates.EMPTY});
+}
+
+function generateColorMatrix(steps: number) {
+  return () => {
+    return createGradiant({ start: '#ffe8e8', end: '#FF0000', steps: steps - 2 });
+  }
 }
 
 export default App;
---
+++ src/utils/colors.js
@@ -0,0 +1,133 @@
+// constants for switch/case checking representation type
+const HEX = 1;
+const RGB = 2;
+const RGBA = 3;
+
+// get the string representation 
+// type and set it on the element (HEX/RGB/RGBA)
+function getType(val) {
+  if (val.indexOf('#') > -1) return HEX;
+  if (val.indexOf('rgb(') > -1) return RGB;
+  if (val.indexOf('rgba(') > -1) return RGBA;
+}
+
+// process the value irrespective of representation type
+function processValue(dataType, value) {
+  switch (dataType) {
+    case HEX:
+      {
+        return processHEX(value);
+      }
+    case RGB:{
+      return processRGB(value);
+    }
+    case RGBA:{
+      return processRGB(value);
+    }
+
+    default:
+      return processHEX(value);
+      
+  }
+}
+
+//return a workable RGB int array [r,g,b] from rgb/rgba representation
+function processRGB(val){
+  var rgb = val.split('(')[1].split(')')[0].split(',');
+  alert(rgb.toString());
+  return [
+      parseInt(rgb[0],10),
+      parseInt(rgb[1],10),
+      parseInt(rgb[2],10)
+  ];
+}
+
+//return a workable RGB int array [r,g,b] from hex representation
+function processHEX(val) {
+  var r, g, b;
+  //does the hex contain extra char?
+  var hex = (val.length >6)?val.substr(1, val.length - 1):val;
+  // is it a six character hex?
+  if (hex.length > 3) {
+
+    //scrape out the numerics
+    r = hex.substr(0, 2);
+    g = hex.substr(2, 2);
+    b = hex.substr(4, 2);
+
+    // if not six character hex,
+    // then work as if its a three character hex
+  } else {
+
+    // just concat the pieces with themselves
+    r = hex.substr(0, 1) + hex.substr(0, 1);
+    g = hex.substr(1, 1) + hex.substr(1, 1);
+    b = hex.substr(2, 1) + hex.substr(2, 1);
+
+  }
+  // return our clean values
+    return [
+      parseInt(r, 16),
+      parseInt(g, 16),
+      parseInt(b, 16)
+    ]
+}
+
+function createGradiant({ start, end, steps }) {
+    var val1RGB = processValue(getType(start), start);
+    var val2RGB = processValue(getType(end), end);
+    var colors = [
+      // somewhere to dump gradient
+    ];
+
+    //the number of steps in the gradient
+    var stepsInt = parseInt(steps, 10);
+    //the percentage representation of the step
+    var stepsPerc = 100 / (stepsInt+1);
+
+    // diffs between two values 
+    var valClampRGB = [
+      val2RGB[0] - val1RGB[0],
+      val2RGB[1] - val1RGB[1],
+      val2RGB[2] - val1RGB[2]
+    ];
+  
+    // build the color array out with color steps
+    for (var i = 0; i < stepsInt; i++) {
+      var clampedR = (valClampRGB[0] > 0) 
+      ? pad((Math.round(valClampRGB[0] / 100 * (stepsPerc * (i + 1)))).toString(16), 2) 
+      : pad((Math.round((val1RGB[0] + (valClampRGB[0]) / 100 * (stepsPerc * (i + 1))))).toString(16), 2);
+      
+      var clampedG = (valClampRGB[1] > 0) 
+      ? pad((Math.round(valClampRGB[1] / 100 * (stepsPerc * (i + 1)))).toString(16), 2) 
+      : pad((Math.round((val1RGB[1] + (valClampRGB[1]) / 100 * (stepsPerc * (i + 1))))).toString(16), 2);
+      
+      var clampedB = (valClampRGB[2] > 0) 
+      ? pad((Math.round(valClampRGB[2] / 100 * (stepsPerc * (i + 1)))).toString(16), 2) 
+      : pad((Math.round((val1RGB[2] + (valClampRGB[2]) / 100 * (stepsPerc * (i + 1))))).toString(16), 2);
+      colors[i] = [
+        '#',
+        clampedR,
+        clampedG,
+        clampedB
+      ].join('');
+    }
+
+    colors.unshift(start);
+    colors.push(end);
+  
+    return colors;
+  }
+  /**
+   * padding function:
+   * cba to roll my own, thanks Pointy!
+   * ==================================
+   * source: http://stackoverflow.com/questions/10073699/pad-a-number-with-leading-zeros-in-javascript
+   */
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+export default createGradiant;
