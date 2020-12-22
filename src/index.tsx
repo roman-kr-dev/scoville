51c176e264d3091a03fa8616424fd80680936488
Roman Kr <roman7@gmail.com>
2020-12-22 20:04:08 +0200

    revert route to hash

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
 import Basic from './versions/basic/App';
-import TwoD from './versions/2d/App';;
+import TwoD from './versions/2d/App';
 
 ReactDOM.render(
   <React.StrictMode>
